25645658867984880695c7552dfa8684db56421b
Sergey <33550578+leviserg@users.noreply.github.com>
2024-05-28 18:49:44 +0300

    Add files via upload

---
+++ ControlEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SwitchCaseEnum
+{
+    public enum ControlEnum
+    {
+        [Display(Name = "Add")]
+        Add = 1,
+
+        [Display(Name = "Subtract")]
+        Subtract,
+
+        [Display(Name = "Mulptiply")]
+        Mulptiply,
+
+        [Display(Name = "Divide")]
+        Divide,
+
+        [Display(Name = "Square")]
+        Square
+    }
+}
---
+++ ControlEnumOperations.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SwitchCaseEnum
+{
+    public static class ControlEnumOperations
+    {
+        public static Dictionary<ControlEnum, Func<int, int, int>> operations = new() {
+            { ControlEnum.Add, (a, b) => a + b },
+            { ControlEnum.Subtract, (a, b) => a - b },
+            { ControlEnum.Mulptiply, (a, b) => a * b },
+            { ControlEnum.Divide, (a, b) => a / b },
+        };
+
+        public static int Execute (ControlEnum control, int a, int b)
+        {
+            return operations[control](a, b);
+            /*
+                if (operations.TryGetValue(control, out var operation))
+                {
+                    return operation(a, b);
+                }
+                throw new NotImplementedException($"Operation {control} is not implemented");
+             */
+        }
+
+        public static int ExecuteLambda(this ControlEnum controlEnum, int a, int b)
+        {
+            return controlEnum switch
+            {
+                ControlEnum.Add => a + b,
+                ControlEnum.Subtract => a - b,
+                ControlEnum.Mulptiply => a * b,
+                ControlEnum.Divide => a / b
+            };
+        }
+    }
+}
